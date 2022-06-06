c0b82b7d86daedebcf276c7819fb38a7237ebf78
Shankar Kakumani <shankarchowdary7545@gmail.com>
2022-06-06 08:36:53 +0530

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Shankar Chowdary on 06/06/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
